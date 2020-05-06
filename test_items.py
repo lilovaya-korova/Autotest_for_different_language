0692b454c3051ca578cf658103295e8e0767f53b
tanja.labchuk <tanja.labchuk@gmail.com>
2020-05-06 15:08:15 +0300

    Update assert in test_items.py

--- test_items.py
+++ test_items.py
@@ -3,5 +3,5 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_button_availability(browser):
     browser.get(link)
-    assert browser.find_element_by_css_selector(".btn-add-to-basket"), "Button not find"
+    assert len(browser.find_elements_by_css_selector(".btn-add-to-basket"))>0, "Button not find"
     #time.sleep(10) #uncomment for checking
